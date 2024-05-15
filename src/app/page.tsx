48c84c85088067d76f9784e29c90269d404dfb1f
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-05-15 23:03:19 +0300

    update photos, update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import type { Metadata } from 'next'
-import PageWrapper from '@/components/PageWrapper'
-import PhotoGallery from '@/components/PhotoGallery'
+import type { Metadata } from 'next';
+import PageWrapper from '@/components/PageWrapper';
+import PhotoGallery from '@/components/PhotoGallery';
 
 export const metadata: Metadata = {
-    title: 'Tetiana Andronenko',
-    description: 'Tetiana Andronenko',
-}
+  title: 'Tetiana Andronenko',
+  description: 'Tetiana Andronenko - model from Ukraine.',
+  keywords:
+    'model, photo model, model agency, models over 30, fashion model, beautiful models, beautiful model.',
+};
 
 export default function Home() {
-    return (
-        <>
-             <PageWrapper>
-                <PhotoGallery />
-            </PageWrapper>
-        </>
-    )
+  return (
+    <>
+      <PageWrapper>
+        <PhotoGallery />
+      </PageWrapper>
+    </>
+  );
 }
