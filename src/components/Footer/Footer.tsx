330a8cf80f1a6b872b86892edb08c8013d887392
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-07-26 14:39:07 +0300

    Add copyright, update styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
+import { useTranslations } from 'next-intl';
 import css from './Footer.module.css';
 
 type Props = {};
 
 const Footer = (props: Props) => {
+  const t = useTranslations('Footer');
+
   return (
-    <div className={css.footer}>&copy;&nbsp;Tetiana Andronenko&nbsp;2024</div>
+    <div className={css.footer}>
+      <p className={css.name}>&copy;&nbsp;Tetiana Andronenko&nbsp;2024</p>
+      <p className={css.copyright}>{t('copyright')}</p>
+    </div>
   );
 };
 
